febc0f7af3187236ca8229a76458bd108c70e794
sadrulj <jamil.sadrul@gmail.com>
2021-10-30 08:04:12 +0600

    commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ediyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -15,7 +20,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("database connected");
+    const database = client.db("tourismDbUser");
+    const guidesCollection = database.collection("guide_db");
+    const servicesCollection = database.collection("services_db");
+
+    // GET API
+    app.get("/guides", async (req, res) => {
+      const cursor = guidesCollection.find({});
+      const guides = await cursor.toArray();
+      res.send(guides);
+    });
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
   } finally {
     // await client.close()
   }
